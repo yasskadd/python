569ab89f79f220b06e89c5c43f99ac3a6d243b8d
yasskadd <71228349+yasskadd@users.noreply.github.com>
2022-10-08 15:47:08 -0400

    changed test format and added test to kill mutant 4

--- tests/test_mutants.py
+++ tests/test_mutants.py
@@ -1,24 +1,29 @@
 import pytest
-from mock import patch
-from unittest.mock import MagicMock, patch
+import unittest
+import mock
 from ipinfo import Handler
 from ipinfo.exceptions import RequestQuotaExceededError
+from ipinfo.handler_utils import get_headers
 
 
+class Test_Mutants(unittest.TestCase):
+    def setUp(self):
+        self.token = "mytesttoken"
 
-def test_init_mutant1():
-    token = "mytesttoken"
-    dictMock = {"request_options": {"test": 1}}
-    handler = Handler(token, **dictMock)
-    assert handler.request_options["timeout"] == 2
+    def test_init_mutant1(self):
+        dictMock = {"request_options": {"test": 1}}
+        handler = Handler(self.token, **dictMock)
+        self.assertEqual(handler.request_options["timeout"], 2)
 
-def test_getDetails_mutant2():
-    token = "mytesttoken"
-    handler = Handler(token)
-    with patch('requests.get') as mock_request:
-        mock_request.return_value.status_code = 429
+    @mock.patch('requests.get')
+    def test_getDetails_mutant2(self, mock_requests):
+        handler = Handler(self.token)
+        mock_requests.return_value.status_code = 429
         with pytest.raises(RequestQuotaExceededError):
             handler.getDetails()
 
+    def test_getHeaders_mutant4(self):
+        expected = 'Bearer ' + self.token
+        self.assertEqual(get_headers(self.token)['authorization'], expected)
 
 
